29aa632e11183979ae8a28fdd2abb92eeb626084
Sos1ska <72695378+Sos1ska@users.noreply.github.com>
2023-04-01 09:14:33 +0300

    Add files via upload

---
+++ source/__eng__.py
@@ -0,0 +1,6 @@
+class Buttons:
+    out = ["Break IP", "Break MAC", "Break Number Phone", "Exit"]
+class Labels:
+    out = ["IP-Address", "MAC-Address", "NumberPhone"]
+class Banners:
+    out = ["Answer"]
---
+++ source/__rus__.py
@@ -0,0 +1,6 @@
+class Buttons:
+    out = ["Пробить IP", "Пробить MAC", "Пробить номер телефона", "Выход"]
+class Labels:
+    out = ["IP-Адрес", "MAC-Адрес", "Номер телефона"]
+class Banners:
+    out = ["Ответ"]
